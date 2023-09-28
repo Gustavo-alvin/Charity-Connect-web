fb6fbe42fa0a94488e1fe7c52f9027dedd2abacd
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2023-09-28 18:56:37 -0300

    finalizado :)

--- script/cpf.js
+++ script/cpf.js
@@ -1,39 +1,22 @@
-function validarCPF() {
-    const cpf = document.getElementById("input3").value.replace(/\D/g, ''); // Remove caracteres não numéricos
-    if (cpf.length !== 11 || !validarDigitos(cpf)) {
-        document.getElementById("resultado").textContent = "CPF inválido";
-    } else {
-        document.getElementById("resultado").textContent = "CPF válido";
-    }
+function TestarCPF(strCPF){
+    var soma;
+    var resto;
+    soma=0;
+    if (strCPF=="00000000000") return false;
+
+    for (i=1; i<=9; i++) soma = soma + parseInt(strCPF.subtring(i-1, i)) * (11 - 1);
+    resto =(soma * 10) % 11;
+
+    if((resto == 10) || (resto == 11)) resto=0;
+    if(resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    soma = 0;
+        for (i =1; i <= 10; i++) soma = soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+        resto = (soma * 10) % 11;
+
+        if ((resto == 10) || (resto == 11)) resto = 0;
+        if (resto != parseInt(strCPF.substring(10, 11))) return false;
+        return true;
 }
-
-function validarDigitos(cpf) {
-    const cpfArray = cpf.split('').map(Number);
-    const somaPrimeirosDigitos = cpfArray.slice(0, 9).reduce((acumulado, valor, indice) => acumulado + valor * (10 - indice), 0);
-    const primeiroDigitoVerificador = (somaPrimeirosDigitos * 10) % 11;
-
-    if (primeiroDigitoVerificador === 10 || primeiroDigitoVerificador === 11) {
-        if (primeiroDigitoVerificador !== cpfArray[9]) {
-            return false;
-        }
-    } else {
-        if (primeiroDigitoVerificador !== cpfArray[9]) {
-            return false;
-        }
-    }
-
-    const somaSegundosDigitos = cpfArray.slice(0, 10).reduce((acumulado, valor, indice) => acumulado + valor * (11 - indice), 0);
-    const segundoDigitoVerificador = (somaSegundosDigitos * 10) % 11;
-
-    if (segundoDigitoVerificador === 10 || segundoDigitoVerificador === 11) {
-        if (segundoDigitoVerificador !== cpfArray[10]) {
-            return false;
-        }
-    } else {
-        if (segundoDigitoVerificador !== cpfArray[10]) {
-            return false;
-        }
-    }
-
-    return true;
-}
+var strCPF = "12345678909";
+alert(TestarCPF(strCPF));
