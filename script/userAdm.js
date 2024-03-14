2129c60268b28fbcf69593abaa9959be5fa70d35
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-03-14 15:49:00 -0300

    kk

--- script/userAdm.js
+++ script/userAdm.js
@@ -57,6 +57,7 @@ const users = [usuario1,usuario2,usuario3,usuario4,usuario5,usuario6,usuario7]
 
 function mudarUser() {
     let ong = document.getElementById("title-ong");
+    let repre = document.getElementById("title-representante");
     let ong1 = document.getElementById("title-ong1");
     let ong2 = document.getElementById("title-ong2");
     let ong3 = document.getElementById("title-ong3");
@@ -66,17 +67,39 @@ function mudarUser() {
     let nome = document.getElementById("nome");
     let email= document.getElementById("email");
     let senha = document.getElementById("senha");
+    let nome2 = document.getElementById("nome2");
+    let email2= document.getElementById("email2");
+    let senha2 = document.getElementById("senha2");
+    let nome3 = document.getElementById("nome3");
+    let email3= document.getElementById("email3");
+    let senha3 = document.getElementById("senha3");
+    let user5 = document.getElementById("user5");
+    let user6 = document.getElementById("user6");
+    let user7 = document.getElementById("user7");
 
     ong.remove();
+    user5.remove();
+    user6.remove();
+    user7.remove();
     ong1.remove();
     ong2.remove();
     ong3.remove();
     ong4.remove();
     ong5.remove();
     ong6.remove();
+
+    repre.innerHTML = "Nomes";
     nome.innerHTML = "Jay jo";
     email.innerHTML = "WindBreaker@gmail.com";
     senha.innerHTML = "IloveBikes";
+
+    nome2.innerHTML = "Dom Kang";
+    email2.innerHTML = "SpeedForce@gmail.com";
+    senha2.innerHTML = "MafiaDosCachimbinhas";
+
+    nome3.innerHTML = "Shelly Scott";
+    email3.innerHTML = "HummingBirdCrew@gmail.com";
+    senha3.innerHTML = "IloveJayJO";
 
 
 }
