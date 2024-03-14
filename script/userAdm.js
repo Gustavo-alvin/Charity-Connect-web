339ccb1c107b7046b835eb0608d4b01ec2cfabc3
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-03-14 20:21:33 -0300

    i am crazy?

--- script/userAdm.js
+++ script/userAdm.js
@@ -1,116 +1,86 @@
-const usuario1= {
-    nome: 'Cauã ',
-    email: 'IloveKaneyeWest@gmail.com',
-    senha: 'TaylorSwift'
-};
+let title = document.querySelectorAll('#title-categoria .item');
+let item = document.querySelectorAll('#info .item');
+let item2 = document.querySelectorAll('#info2 .item');
+let item3 = document.querySelectorAll('#info3 .item');
+let item4 = document.querySelectorAll('#info4 .item');
+let item5 = document.querySelectorAll('#info5 .item');
+let item6 = document.querySelectorAll('#info6 .item');
 
-const usuario2= {
-    nome: 'Alves',
-    email: 'contato.alvingustavo@gmail.com',
-    senha: 'WindBreaker'
-};
-
-const usuario3= {
-    nome: 'Gomes',
-    email: 'IloveKaneyeWest@gmail.com',
-    senha: 'TaylorSwift'
-};
-
-const usuario4= {
-    nome: 'Leandro',
-    email: 'IloveKaneyeWest@gmail.com',
-    senha: 'TaylorSwift'
-};
-
-const usuario5= {
-    nome: 'Nathalia',
-    email: 'IloveKaneyeWest@gmail.com',
-    senha: 'TaylorSwift'
-};
-
-const usuario6= {
-    nome: 'Ruana',
-    email: 'IloveKaneyeWest@gmail.com',
-    senha: 'TaylorSwift'
-};
-
-const usuario7= {
-    nome: 'Wellington',
-    email: 'IloveKaneyeWest@gmail.com',
-    senha: 'TaylorSwift'
-};
-
-const users = [usuario1,usuario2,usuario3,usuario4,usuario5,usuario6,usuario7]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function mudarUser() {
-    let ong = document.getElementById("title-ong");
-    let repre = document.getElementById("title-representante");
-    let ong1 = document.getElementById("title-ong1");
-    let ong2 = document.getElementById("title-ong2");
-    let ong3 = document.getElementById("title-ong3");
-    let ong4 = document.getElementById("title-ong4");
-    let ong5 = document.getElementById("title-ong5");
-    let ong6 = document.getElementById("title-ong6");
-    let nome = document.getElementById("nome");
-    let email= document.getElementById("email");
-    let senha = document.getElementById("senha");
-    let nome2 = document.getElementById("nome2");
-    let email2= document.getElementById("email2");
-    let senha2 = document.getElementById("senha2");
-    let nome3 = document.getElementById("nome3");
-    let email3= document.getElementById("email3");
-    let senha3 = document.getElementById("senha3");
-    let user5 = document.getElementById("user5");
-    let user6 = document.getElementById("user6");
-    let user7 = document.getElementById("user7");
-
-    ong.remove();
-    user5.remove();
-    user6.remove();
-    user7.remove();
-    ong1.remove();
-    ong2.remove();
-    ong3.remove();
-    ong4.remove();
-    ong5.remove();
-    ong6.remove();
-
-    repre.innerHTML = "Nomes";
-    nome.innerHTML = "Jay jo";
-    email.innerHTML = "WindBreaker@gmail.com";
-    senha.innerHTML = "IloveBikes";
-
-    nome2.innerHTML = "Dom Kang";
-    email2.innerHTML = "SpeedForce@gmail.com";
-    senha2.innerHTML = "MafiaDosCachimbinhas";
-
-    nome3.innerHTML = "Shelly Scott";
-    email3.innerHTML = "HummingBirdCrew@gmail.com";
-    senha3.innerHTML = "IloveJayJO";
-
-
-}
 
 function mudarOngs() {
+console.log(item);
+title[0].textContent = " ONG";
+title[1].textContent = "Representante";
+title[2].textContent = "E-mail";
+title[3].textContent = "Senha";
 
-    ong.innerHTML.add();
-    ong1.add();
-    ong2.add();
-    ong3.add();
-    ong4.add();
-    ong5.add();
-    ong6.add();
+item[0].textContent = " LIFEOFPABLO";
+item[1].textContent = "	CAUÃ";
+item[2].textContent = "	C@HORSE.COM";
+item[3].textContent = "FREDDIEMERCURY";
+
+item2[0].textContent = "GRADUATION";
+item2[1].textContent = " GUSTAVO";
+item2[2].textContent = " G@HORSE.COM";
+item2[3].textContent = " AMYWINEHOUSE";
+
+item3[0].textContent = "YEEZUS";
+item3[1].textContent = "LEANDRO";
+item3[2].textContent = "L@HORSE.COM";
+item3[3].textContent = "FRANKSINATRA";
+
+item4[0].textContent = " YE";
+item4[1].textContent = "NATHALIA";
+item4[2].textContent = "N@HORSE.COM";
+item4[3].textContent = "OZZYOSBOURNE";
+
+item5[0].textContent = " DONDA";
+item5[1].textContent = "RUAN";
+item5[2].textContent = "I@HORSE.COM";
+item5[3].textContent = "MICHAELJACKSON";
+
+item6[0].textContent = " WATCHTHRONE";
+item6[1].textContent = "WELLINGTON";
+item6[2].textContent = "W@HORSE.COM";
+item6[3].textContent = " JOHNLENNON";
 }
+
+function mudarUser() {
+    title[0].textContent = ""
+    title[1].textContent = "Nome";
+    title[2].textContent = "E-mail";
+    title[3].textContent = "Senha";
+
+    item[0].textContent = " ";
+    item[1].textContent = "	JAY JO";
+    item[2].textContent = "	WINDBREAKER@GMAIL.COM";
+    item[3].textContent = "ILOVEBIKES";
+
+    item2[0].textContent = " ";
+    item2[1].textContent = "DOM KANG";
+    item2[2].textContent = "SPEEDFORCE@GMAIL.COM";
+    item2[3].textContent = "MAFIADOSCACHIMBINHAS";
+
+    item3[0].textContent = " ";
+    item3[1].textContent = "SHELLY SCOTT";
+    item3[2].textContent = "HUMMINGBIRD@GMAIL.COM";
+    item3[3].textContent = "IloveJayJO";
+
+    item4[0].textContent = " ";
+    item4[1].textContent = "OWEN KNIGHT";
+    item4[2].textContent = "LIGHTCAVALRY@GMAIL.COM";
+    item4[3].textContent = "ILOVESHELLY";
+
+    item5[0].textContent = " ";
+    item5[1].textContent = "JOKER";
+    item5[2].textContent = "SABBATH@GMAIL.COM";
+    item5[3].textContent = "ILOVECATS";
+
+    item6[0].textContent = " ";
+    item6[1].textContent = "Kaneshiro";
+    item6[2].textContent = "KAZUMA@GMAIL.COM";
+    item6[3].textContent = "ILOVECROE";
+
+
+}
+ 
