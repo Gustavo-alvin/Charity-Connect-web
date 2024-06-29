c64e83b9272ee6d7b086a7a6e093c7bb6847dec7
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-06-29 14:43:08 -0300

    web finalizado(eu espero)

--- public/script/userAdm.js
+++ public/script/userAdm.js
@@ -4,18 +4,27 @@
 function mostrarGerenciamento() {
     var gerenciamentoDiv = document.getElementById('gerenciamento');
     var faleConoscoDiv = document.getElementById('fale-conosco');
+    var container = document.querySelector('.container');
+    var containerFaleConosco = document.querySelector('.container-fale-conosco');
 
     gerenciamentoDiv.style.display = 'block';
     faleConoscoDiv.style.display = 'none';
+
+    container.classList.remove('container-fale-conosco');
 }
 
 // Função para mostrar a seção de Fale Conosco e esconder Gerenciamento de Usuários
 function mostrarFaleConosco() {
     var gerenciamentoDiv = document.getElementById('gerenciamento');
     var faleConoscoDiv = document.getElementById('fale-conosco');
+    var container = document.querySelector('.container');
+    var containerFaleConosco = document.querySelector('.container-fale-conosco');
 
     gerenciamentoDiv.style.display = 'none';
-    faleConoscoDiv.style.display = 'block';
+    faleConoscoDiv.style.display = 'flex';
+
+    container.classList.add('container-fale-conosco');
+    
 }
 
 // Configura os eventos de clique nos botões ao carregar a página
